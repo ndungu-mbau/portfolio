a8ae79fad1a45d32300462de31dfeb8e0c2ed77d
Nelson Mbau <mbau.ndungu@gmail.com>
2025-06-17 10:49:20 +0300

    feat: set github as auth provider

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { db } from "~/server/db";
 import {
@@ -38,7 +38,7 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    GithubProvider,
     /**
      * ...add more providers here.
      *
